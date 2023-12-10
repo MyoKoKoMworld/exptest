83e5e64e1aa5d10c0fa2311224d575574165385d
M world <myokokomworld@gmail.com>
2023-12-10 21:00:08 +0630

    sec commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+
 app.get("/", (req, res) => {
   res.json({ hello: "hello user" });
 });
@@ -9,4 +11,4 @@ app.get("/users", (req, res) => {
   res.send("hello");
 });
 
-app.listen(3000);
+app.listen(process.env.PORT);
